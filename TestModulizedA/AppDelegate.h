7b5322b78b0d8ac92772f23a2b4860b9c3d3c535
zengpengyuan <pyzeng@sina.cn>
2019-04-11 09:30:07 +0800

    add project

---
+++ TestModulizedA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestModulizedA
+//
+//  Created by ZengPengYuan on 2019/4/11.
+//  Copyright © 2019 ZengPengYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestModulizedA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestModulizedA
+//
+//  Created by ZengPengYuan on 2019/4/11.
+//  Copyright © 2019 ZengPengYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
